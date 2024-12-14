6f151561e013d612a9791848205c1ed52b45447e
Z125851 <Z125851@outlook.com>
2024-12-14 12:49:16 +0300

    Add files via upload

---
+++ Базовые структуры данных ДЗ.py
@@ -0,0 +1,18 @@
+#"1st program"
+print((9**0,5)*5)
+#"2st program"
+print(9.99>9,98 and 1000 != 1000,1)
+#"3rd program"
+print(2*2+2)
+print(2*(2+2))
+print (2*2+2) == (2*(2+2))
+#"4st program"
+print(123.456)
+print(int(123.456))
+print(float(123.456))
+print(type(float(123.456)))
+print(123.456*10)
+print(int(1234.56))
+print(1234/4)
+print(1234/308.5)
+print(int(4.0))
